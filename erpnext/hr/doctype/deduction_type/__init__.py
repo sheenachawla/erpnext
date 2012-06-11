619bc980afb4c48fc228ecf1b302f4ff3cc4263d
Nabin Hait <nabinhait@gmail.com>
2012-06-11 18:13:26 +0530

    directory removed from filesystem for default records

---
+++ erpnext/hr/doctype/deduction_type/__init__.py
@@ -0,0 +1,9 @@
+install_docs = [
+	{'doctype': 'Deduction Type', 'name': 'Income Tax', 'description': 'Income Tax', \
+	'is_active': 'Yes', 'module': 'Payroll', 'deduction_name': 'Income Tax'},
+	{'doctype': 'Deduction Type', 'name': 'Professional Tax', 'description': 'Professional Tax', \
+		'is_active': 'Yes', 'module': 'Payroll', 'deduction_name': 'Professional Tax'},
+	{'doctype': 'Deduction Type', 'name': 'Provident Fund', 'description': 'Provident fund', \
+		'is_active': 'Yes', 'module': 'Payroll', 'deduction_name': 'Provident Fund'}
+	
+]
---
+++ erpnext/hr/doctype/earning_type/__init__.py
@@ -0,0 +1,6 @@
+install_docs = [
+	{'doctype': 'Earning Type', 'name': 'Basic', 'description': 'Basic', 'is_active': 'Yes', \
+		'earning_name': 'Basic', 'taxable': 'Yes', 'module': 'Payroll'},
+	{'doctype': 'Earning Type', 'name': 'House Rent Allowance', 'description': 'House Rent Allowance', \
+		'is_active': 'Yes', 'earning_name': 'House Rent Allowance', 'taxable': 'No', 'module': 'Payroll'}
+]
