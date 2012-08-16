dd54826bebede90cdde49de1336ea97988303482
Nabin Hait <nabinhait@gmail.com>
2012-08-16 18:05:49 +0530

    fixed the patch

--- erpnext/patches/12_08/accounts_cleanup.py
+++ erpnext/patches/12_08/accounts_cleanup.py
@@ -8,8 +8,10 @@ def remove_doctypes():
 	from webnotes.model import delete_doc
 	dt_list = ['Account Balance', 'Period', 'Reposting Tool', 'Lease Agreement', 'Lease Installment']
 	for dt in dt_list:
-		delete_doc('DocType', dt)	
-		webnotes.conn.sql("drop table `tab%s`" % dt)
+		delete_doc('DocType', dt)
+		webnotes.conn.commit()
+		webnotes.conn.sql("drop table if exists `tab%s`" % dt)
+		webnotes.conn.begin()
 		
 def set_fy_end_date():
 	for fy in webnotes.conn.sql("select name, year_start_date, year_end_date from `tabFiscal Year`", as_dict=1):
