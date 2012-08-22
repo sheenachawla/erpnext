1e80939a9dd437db2a024a749c9a49d3a0c9d576
Rushabh Mehta <rmehta@gmail.com>
2012-08-22 17:52:06 +0530

    added fullcalendar 3rd party library

--- erpnext/utilities/page/calendar/calendar.py
+++ erpnext/utilities/page/calendar/calendar.py
@@ -1,1 +1,13 @@
 from __future__ import unicode_literals
+
+import webnotes
+
+@webnotes.whitelist()
+def get_events():
+	import datetime
+	
+	return webnotes.conn.sql("""select name as `id`, description as title, 
+		event_date as `start` from tabEvent 
+		where event_date between %s and %s 
+		and (event_type='Public' or owner=%s)""", (webnotes.form.start, webnotes.form.end, 
+			webnotes.session['user']), as_dict=1)	
