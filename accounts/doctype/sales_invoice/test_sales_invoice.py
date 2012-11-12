f2aa311bb505c524447512e5cc2e520ab1a39366
Anand Doshi <anand@iwebnotes.com>
2012-11-12 20:22:10 +0530

    test cases for inclusive tax calculations

--- accounts/doctype/sales_invoice/test_sales_invoice.py
+++ accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -19,6 +19,7 @@ from __future__ import unicode_literals
 import unittest
 import webnotes
 import webnotes.model
+from webnotes.model.doclist import DocList
 from webnotes.utils import nowdate
 from accounts.utils import get_fiscal_year
 
@@ -38,7 +39,8 @@ def load_data():
 	# create customer
 	webnotes.insert({"doctype": "Customer", "customer_name": "West Wind Inc.",
 		"customer_type": "Company", "territory": "Default",
-		"customer_group": "Default Customer Group", "company": company})
+		"customer_group": "Default Customer Group", "company": company,
+		"credit_days": 0, "credit_limit": 0})
 	
 	webnotes.insert({"doctype": "Account", "account_name": "Excise Duty",
 		"parent_account": "Tax Assets - %s" % abbr, "company": company,
@@ -147,9 +149,10 @@ class TestSalesInvoice(unittest.TestCase):
 		load_data()
 		webnotes.conn.set_value("Global Defaults", None, "automatic_inventory_accounting", 1)
 
-	def test_purchase_invoice(self):
+	def test_sales_invoice(self):
 		from webnotes.model.doclist import DocList
-		controller = webnotes.insert(DocList(sales_invoice_doclist))
+		doclist = [] + [d.copy() for d in sales_invoice_doclist]
+		controller = webnotes.insert(DocList(doclist))
 		controller.load_from_db()
 		dl = controller.doclist
 
@@ -197,15 +200,101 @@ class TestSalesInvoice(unittest.TestCase):
 			self.assertEqual(tax.account_head, expected_values[i][0])
 			self.assertEqual(tax.tax_amount, expected_values[i][1])
 			self.assertEqual(tax.total, expected_values[i][2])
-	
-	def test_purchase_invoice_with_inclusive_tax(self):
-		sales_invoice_doclist[1]["print_rate"] = 62.683
-		sales_invoice_doclist[2]["print_rate"] = 191
+			
+	def test_inclusive_rate_validations(self):
+		doclist = [] + [d.copy() for d in sales_invoice_doclist]
+		doclist[1]["print_rate"] = 62.5
+		doclist[2]["print_rate"] = 191
 		for i in [3, 5, 6, 7, 8, 9]:
-			sales_invoice_doclist[i]["included_in_print_rate"] = 1
+			doclist[i]["included_in_print_rate"] = 1
 		
-		from webnotes.model.doclist import DocList
-		controller = webnotes.insert(DocList(sales_invoice_doclist))
+		# tax type "Actual" cannot be inclusive
+		self.assertRaises(webnotes.ValidationError, webnotes.insert,
+			DocList(doclist))
+		
+		doclist[3]["included_in_print_rate"] = 0
+		# taxes above included type 'On Previous Row Total' should also be included
+		self.assertRaises(webnotes.ValidationError, webnotes.insert,
+			DocList(doclist))
+	
+	def test_sales_invoice_with_inclusive_tax(self):
+		doclist = [
+			# parent
+			{
+				"doctype": "Sales Invoice", 
+				"debit_to": "West Wind Inc. - %s" % abbr,
+				"customer_name": "West Wind Inc.",
+				"naming_series": "INV", "posting_date": nowdate(),
+				"company": company,
+				"fiscal_year": webnotes.conn.get_default("fiscal_year"), 
+				"currency": webnotes.conn.get_default("currency"), "exchange_rate": 1.0,
+				"grand_total_print": 0
+			},
+			# items
+			{
+				"doctype": "Sales Invoice Item", "warehouse": "Default Warehouse",
+				"item_code": "Home Desktop 100", "qty": 10, "print_rate": 62.503,
+				"parentfield": "sales_invoice_items",
+				"uom": "Nos", "item_tax_rate": json.dumps({"Excise Duty - %s" % abbr: 10})
+			},
+			{
+				"doctype": "Sales Invoice Item", "warehouse": "Default Warehouse",
+				"item_code": "Home Desktop 200", "qty": 5, "print_rate": 190.6608,
+				"parentfield": "sales_invoice_items",
+				"uom": "Nos"
+			},
+			# taxes
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "Excise Duty - %s" % abbr, "rate": 12,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Amount",
+				"account_head": "Education Cess - %s" % abbr, "rate": 2, "row_id": 1,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Amount",
+				"account_head": "S&H Education Cess - %s" % abbr, "rate": 1, "row_id": 1,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges", 
+				"charge_type": "On Previous Row Total",
+				"account_head": "CST - %s" % abbr, "rate": 2, "row_id": 3,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1,
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "VAT - Test - %s" % abbr, "rate": 12.5,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "Customs Duty - %s" % abbr, "rate": 10,
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "Actual",
+				"account_head": "Shipping Charges - %s" % abbr, "tax_amount_print": 100, 
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Total",
+				"account_head": "Discount - %s" % abbr, "rate": -10, "row_id": 7,
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+		]
+		
+		controller = webnotes.insert(DocList(doclist))
 		controller.load_from_db()
 		dl = controller.doclist
 
@@ -213,18 +302,18 @@ class TestSalesInvoice(unittest.TestCase):
 		expected_values = [
 			{
 				"item_code": "Home Desktop 100",
-				"print_ref_rate": 62.683,
+				"print_ref_rate": 62.503,
 				"discount": 0,
-				"print_amount": 626.83,
+				"print_amount": 625.03,
 				"ref_rate": 50,
 				"rate": 50,
 				"amount": 500
 			},
 			{
 				"item_code": "Home Desktop 200",
-				"print_ref_rate": 191,
+				"print_ref_rate": 190.6608,
 				"discount": 0,
-				"print_amount": 956,
+				"print_amount": 953.3,
 				"ref_rate": 150,
 				"rate": 150,
 				"amount": 750
@@ -236,17 +325,18 @@ class TestSalesInvoice(unittest.TestCase):
 		
 		# test tax amounts and totals
 		expected_values = [
-			["Shipping Charges - %s" % abbr, 100, 1350, 100, 1682.83],
-			["Customs Duty - %s" % abbr, 125, 1475, 125, 1807.83],
-			["Excise Duty - %s" % abbr, 140, 1615, 0, 1807.83],
-			["Education Cess - %s" % abbr, 2.8, 1617.8, 0, 1807.83],
-			["S&H Education Cess - %s" % abbr, 1.4, 1619.2, 0, 1807.83],
-			["CST - %s" % abbr, 32.38, 1651.58, 0, 1807.83],
-			["VAT - Test - %s" % abbr, 156.25, 1807.83, 0, 1807.83],
-			["Discount - %s" % abbr, -180.78, 1627.05, -180.78, 1627.05],
+			["Excise Duty - %s" % abbr, 140, 1390, 0, 1578.33],
+			["Education Cess - %s" % abbr, 2.8, 1392.8, 0, 1578.33],
+			["S&H Education Cess - %s" % abbr, 1.4, 1394.2, 0, 1578.33],
+			["CST - %s" % abbr, 27.88, 1422.08, 0, 1578.33],
+			["VAT - Test - %s" % abbr, 156.25, 1578.33, 0, 1578.33],
+			["Customs Duty - %s" % abbr, 125, 1703.33, 125, 1703.33],
+			["Shipping Charges - %s" % abbr, 100, 1803.33, 100, 1803.33],
+			["Discount - %s" % abbr, -180.33, 1623, -180.33, 1623],
 		]		
 		for i, tax in enumerate(dl.get({"parentfield": "taxes_and_charges"})):
-			print tax.account_head, tax.tax_amount, tax.total, tax.tax_amount_print, tax.total_print
+			# print tax.account_head, tax.tax_amount, tax.total, tax.tax_amount_print, \
+			# 	tax.total_print
 			self.assertEqual(tax.account_head, expected_values[i][0])
 			self.assertEqual(tax.tax_amount, expected_values[i][1])
 			self.assertEqual(tax.total, expected_values[i][2])
@@ -255,85 +345,153 @@ class TestSalesInvoice(unittest.TestCase):
 			
 		# test net total
 		self.assertEqual(dl[0].net_total, 1250)
-		self.assertEqual(dl[0].net_total_print, 1582.83)
+		self.assertEqual(dl[0].net_total_print, 1578.33)
 
 		# # test grand total
-		self.assertEqual(dl[0].grand_total, 1627.05)
-		self.assertEqual(dl[0].grand_total_print, 1627.05)
-	
-	
-	
-	
-	# 		
-	# def test_purchase_invoice_having_zero_amount_items(self):
-	# 	from webnotes.model.doclist import DocList
-	# 	sample_purchase_invoice_doclist = [] + purchase_invoice_doclist
-	# 	
-	# 	# set rate and amount as 0
-	# 	sample_purchase_invoice_doclist[1]["rate"] = 0
-	# 	sample_purchase_invoice_doclist[1]["amount"] = 0
-	# 	sample_purchase_invoice_doclist[2]["rate"] = 0
-	# 	sample_purchase_invoice_doclist[2]["amount"] = 0
-	# 	
-	# 	
-	# 	controller = webnotes.insert(DocList(sample_purchase_invoice_doclist))
-	# 	controller.load_from_db()
-	# 	dl = controller.doclist
-	# 	
-	# 	# test net total
-	# 	self.assertEqual(dl[0].net_total, 0)
-	# 	
-	# 	# test tax amounts and totals
-	# 	expected_values = [
-	# 		["Shipping Charges - %s" % abbr, 100, 100],
-	# 		["Customs Duty - %s" % abbr, 0, 100],
-	# 		["Excise Duty - %s" % abbr, 0, 100],
-	# 		["Education Cess - %s" % abbr, 0, 100],
-	# 		["S&H Education Cess - %s" % abbr, 0, 100],
-	# 		["CST - %s" % abbr, 2, 102],
-	# 		["VAT - Test - %s" % abbr, 0, 102],
-	# 		["Discount - %s" % abbr, -10.2, 91.8],
-	# 	]
-	# 	for i, tax in enumerate(dl.get({"parentfield": "taxes_and_charges"})):
-	# 		# print tax.account_head, tax.tax_amount, tax.total
-	# 		self.assertEqual(tax.account_head, expected_values[i][0])
-	# 		self.assertEqual(tax.tax_amount, expected_values[i][1])
-	# 		self.assertEqual(tax.total, expected_values[i][2])
-	# 	
-	# 	# test item tax amount
-	# 	expected_values = [
-	# 		["Home Desktop 100", 0],
-	# 		["Home Desktop 200", 0]
-	# 	]
-	# 	for i, item in enumerate(dl.get({"parentfield": "purchase_invoice_items"})):
-	# 		self.assertEqual(item.item_code, expected_values[i][0])
-	# 		self.assertEqual(item.valuation_tax_amount, expected_values[i][1])
-	# 	
-	# def test_gl_entries(self):
-	# 	from webnotes.model.doclist import DocList
-	# 	controller = webnotes.insert(DocList(purchase_invoice_doclist))
-	# 	controller.submit()
-	# 	controller.load_from_db()
-	# 	dl = controller.doclist
-	# 	
-	# 	expected_values = {
-	# 		"West Wind Inc. - %s" % abbr : [0, 1512.30],
-	# 		"Shipping Charges - %s" % abbr : [100, 0],
-	# 		"Excise Duty - %s" % abbr : [140, 0],
-	# 		"Education Cess - %s" % abbr : [2.8, 0],
-	# 		"S&H Education Cess - %s" % abbr : [1.4, 0],
-	# 		"CST - %s" % abbr : [29.88, 0],
-	# 		"VAT - Test - %s" % abbr : [156.25, 0],
-	# 		"Discount - %s" % abbr : [0, 168.03],
-	# 		"Stock Received But Not Billed - %s" % abbr : [1475, 0],
-	# 		"Expenses Included In Valuation - %s" % abbr : [0, 225]
-	# 	}
-	# 	gl_entries = webnotes.conn.sql("""select account, debit, credit from `tabGL Entry`
-	# 		where voucher_type = 'Purchase Invoice' and voucher_no = %s""", dl[0].name, as_dict=1)
-	# 	for d in gl_entries:
-	# 		self.assertEqual(d["debit"], expected_values.get(d['account'])[0])
-	# 		self.assertEqual(d["credit"], expected_values.get(d['account'])[1])
-	# 		
+		self.assertEqual(dl[0].grand_total, 1623)
+		self.assertEqual(dl[0].grand_total_print, 1623)
 		
+	def test_usd_sales_invoice_with_inclusive_tax(self):
+		# print
+		# print "-"*80
+		# print "test_usd_sales_invoice_with_inclusive_tax"
+		# print "-"*80
+		
+		# Note: below values were obtained through manual calculation and verified by test
+		
+		doclist = [
+			# parent
+			{
+				"doctype": "Sales Invoice", 
+				"debit_to": "West Wind Inc. - %s" % abbr,
+				"customer_name": "West Wind Inc.",
+				"naming_series": "INV", "posting_date": nowdate(),
+				"company": company,
+				"fiscal_year": webnotes.conn.get_default("fiscal_year"), 
+				"currency": "USD", "exchange_rate": 50.0,
+				"grand_total_print": 0
+			},
+			# items
+			{
+				"doctype": "Sales Invoice Item", "warehouse": "Default Warehouse",
+				"item_code": "Home Desktop 100", "qty": 10, "print_rate": 50,
+				"discount": 10, "parentfield": "sales_invoice_items",
+				"uom": "Nos", "item_tax_rate": json.dumps({"Excise Duty - %s" % abbr: 10})
+			},
+			{
+				"doctype": "Sales Invoice Item", "warehouse": "Default Warehouse",
+				"item_code": "Home Desktop 200", "qty": 5, "print_rate": 150,
+				"discount": 20, "parentfield": "sales_invoice_items",
+				"uom": "Nos"
+			},
+			# taxes
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "Excise Duty - %s" % abbr, "rate": 12,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Amount",
+				"account_head": "Education Cess - %s" % abbr, "rate": 2, "row_id": 1,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Amount",
+				"account_head": "S&H Education Cess - %s" % abbr, "rate": 1, "row_id": 1,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges", 
+				"charge_type": "On Previous Row Total",
+				"account_head": "CST - %s" % abbr, "rate": 2, "row_id": 3,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1,
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "VAT - Test - %s" % abbr, "rate": 12.5,
+				"parentfield": "taxes_and_charges", "included_in_print_rate": 1
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "On Net Total",
+				"account_head": "Customs Duty - %s" % abbr, "rate": 10,
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges", "charge_type": "Actual",
+				"account_head": "Shipping Charges - %s" % abbr, "tax_amount_print": 100, 
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+			{
+				"doctype": "Sales Taxes and Charges",
+				"charge_type": "On Previous Row Total",
+				"account_head": "Discount - %s" % abbr, "rate": -10, "row_id": 7,
+				"parentfield": "taxes_and_charges",
+				"cost_center": "Default Cost Center - %s" % abbr
+			},
+		]
+		
+		controller = webnotes.insert(DocList(doclist))
+		controller.load_from_db()
+		dl = controller.doclist
+
+		# test item values calculation
+		expected_values = [
+			{
+				"item_code": "Home Desktop 100",
+				"print_ref_rate": 55.5556,
+				"discount": 10,
+				"print_amount": 500,
+				"ref_rate": 2222.1156,
+				"rate": 1999.904,
+				"amount": 19999.04
+			},
+			{
+				"item_code": "Home Desktop 200",
+				"print_ref_rate": 187.5,
+				"discount": 20,
+				"print_amount": 750,
+				"ref_rate": 7375.664,
+				"rate": 5900.5312,
+				"amount": 29502.66
+			},
+		]
+		for i, item in enumerate(dl.get({"parentfield": "sales_invoice_items"})):
+			for key, val in expected_values[i].items():
+				self.assertEqual(item.fields.get(key), val)
+		
+		# test tax amounts and totals
+		expected_values = [
+			["Excise Duty - %s" % abbr, 5540.22, 55041.92, 0, 1250],
+			["Education Cess - %s" % abbr, 110.81, 55152.73, 0, 1250],
+			["S&H Education Cess - %s" % abbr, 55.4, 55208.13, 0, 1250],
+			["CST - %s" % abbr, 1104.16, 56312.29, 0, 1250],
+			["VAT - Test - %s" % abbr, 6187.71, 62500, 0, 1250],
+			["Customs Duty - %s" % abbr, 4950.17, 67450.17, 99.01, 1349.01],
+			["Shipping Charges - %s" % abbr, 5000, 72450.17, 100, 1449.01],
+			["Discount - %s" % abbr, -7245.01, 65205.16, -144.9, 1304.11],
+		]		
+		for i, tax in enumerate(dl.get({"parentfield": "taxes_and_charges"})):
+			# print tax.account_head, tax.tax_amount, tax.total, tax.tax_amount_print, \
+			# 	tax.total_print
+			self.assertEqual(tax.account_head, expected_values[i][0])
+			self.assertEqual(tax.tax_amount, expected_values[i][1])
+			self.assertEqual(tax.total, expected_values[i][2])
+			self.assertEqual(tax.tax_amount_print, expected_values[i][3])
+			self.assertEqual(tax.total_print, expected_values[i][4])
+			
+		# test net total
+		self.assertEqual(dl[0].net_total, 49501.7)
+		self.assertEqual(dl[0].net_total_print, 1250)
+
+		# # test grand total
+		self.assertEqual(dl[0].grand_total, 65205.16)
+		self.assertEqual(dl[0].grand_total_print, 1304.11)
+		
+
 	def tearDown(self):
 		webnotes.conn.rollback()
